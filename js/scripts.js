d79de2f0a16a8c367168c7b0b38fd3fb3485a3f0
kellyhatt <61627032+kellyhatt@users.noreply.github.com>
2020-04-18 19:31:04 +0100

    Added js  hamburger menu and smooth scrolling

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+console.log("Welcome to my Portfolio Site!")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+$(document).ready(function() {
+
+  var scrollLink = $('.scroll');
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
+})
